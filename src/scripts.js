f829570acacb30ba34287789fa1909a66a80f769
Wayne Graham <wgraham@clir.org>
2017-12-12 16:39:05 -0500

    Update the title grid
    updates the titles for the grid of content

--- src/scripts.js
+++ src/scripts.js
@@ -120,7 +120,7 @@
 				noscroll = false;
 				enable_scroll();
 			}
-		}, 1200 );
+		}, 1000 );
 	}
 
 	// refreshing the page...
@@ -138,26 +138,3 @@
 	window.addEventListener( 'scroll', scrollPage );
 	trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
 })();
-
-// fix issue for browsers without console
-(function() {
-	var method;
-	var noop = function () {};
-	var methods = [
-		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-		'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-	];
-	var length = methods.length;
-	var console = (window.console = window.console || {});
-
-	while (length--) {
-		method = methods[length];
-
-		// Only stub undefined methods.
-		if (!console[method]) {
-			console[method] = noop;
-		}
-	}
-}());
